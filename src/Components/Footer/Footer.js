5ba9c83504b24560120b4258007f708c73215371
Rakshit <rakshitpanjwani@gmail.com>
2021-12-01 18:09:15 +0530

    fixed font

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,14 +8,14 @@ import {ReactComponent as SIlogo} from '../../assets/icons/silogo.svg';
         render(){
             return (
             <div id="footer" className="d-flex flex-column align-items-center justify-content-center pb-0 mb-0" >
-            <Logo/>
-            <p className="mt-3 text-muted foottext">Ajay Kumar Garg Engineering College, Ghaziabad</p>
+            <Logo className="mt-5"/>
+            <p className="mt-3 mb-5 text-muted foottext">Ajay Kumar Garg Engineering College, Ghaziabad</p>
             <div class="row text-light">
             <div class="col-sm"></div>
   </div>
 
   <div className="hrline"></div>
-  <div className='d-flex align-items-center pt-2 pb-0 justify-content-center footer-bottom text-light si-text'>
+  <div className='d-flex align-items-center pt-2 pb-2 justify-content-center footer-bottom text-light si-text'>
         DESIGNED &amp; DEVELOPED BY : <SIlogo className="silogo" /> <a href="https://silive.in"  rel="noreferrer" className="si" target="_blank">SOFTWARE INCUBATOR</a>
         </div>
             </div>
