0c2355261816963259484d07a013e0aa589ad3be
saksham-tiwari <lucky121saty@gmail.com>
2022-11-05 16:04:28 +0530

    Homepage changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,7 +13,7 @@ function Header(props) {
       <div className='container-lg d-flex justify-content-around align-items-center'>
         <div className='d-flex align-items-center py-3'>
           <img src={logo} width='80px' alt="logo" />
-          <h5 className="ms-4 font-weight-bold name-heading">International Conference on Advancements and Key Challenges in Green Energy and Computing</h5>
+          <h5 className="ms-4 font-weight-bold name-heading">1st IEEE International Conference on Advanced Computing and Emerging Technologies (ACET)</h5>
         </div>
         {/* <div className='ms-5 align-items-center d-none d-md-flex'>
       <div className=' p-2 me-4 eventDate'>Event&nbsp;Date&nbsp;:&nbsp;&nbsp;18&nbsp;-&nbsp;19&nbsp;Feb’&nbsp;2022</div>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -16,9 +16,9 @@ class Home extends Component {
             <div id="bg-green" className="d-inline-block p-2 mt-3">
               INTERNATIONAL CONFERENCE on
             </div>
-            <h5 className="greytext mt-2 ">Advancements and Key Challenges in</h5>
-            <h2 className="text-uppercase greenhead bold-text display-4">Green Energy<br />and Computing</h2>
-            <Calendar /> <h5 className="greytext d-inline m-auto">24-25 Feb 2023</h5>
+            {/* <h5 className="greytext mt-2 ">Advancements and Key Challenges in</h5> */}
+            <h2 className="text-uppercase greenhead bold-text display-4">Advanced Computing and <br />Emerging Technologies (ACET)</h2>
+            <Calendar /> <h5 className="greytext d-inline m-auto">06-07 Oct 2023</h5>
           </div>
           <div className="w-50 d-flex deskicon" ><Desktop className="d-inline-block w-50 m-auto" /></div>
         </div>
@@ -32,18 +32,21 @@ class Home extends Component {
         <div id="date" className="target mt-3">
           <div className="container" >
             <h1 className="mb-3 green-heading">Date and Venue of the Conference</h1>
-            <p>The conference hosted by Department of Electrical and Electronics Engineering, will be held offline at AKGEC, Ghaziabad, Uttar Pradesh, India from February 24-25, 2023. The conference will include regular sessions with poster/oral presentations, global and technical sessions, panel discussions and poster presentations organized in AKGEC.</p>
+            <p>The conference organized by Department of Computer Science & Engineering, will be held offline at AKGEC, Ghaziabad, Uttar Pradesh, India on October 6-7, 2023 in collaboration with IEEE Uttar Pradesh Section. The conference will include regular sessions with poster/oral presentations, global and technical sessions, panel discussions and poster presentations organized in AKGEC.</p>
           </div>
         </div>
 
         <div id="theme" className="target">
           <div className="container mb-5" >
             <h1 className="mb-3 green-heading">Conference Theme</h1>
-            <p>The international conference on Advancements & Key Challenges in Green Energy and Computing (AKGEC-2023) will offer themes in the engineering aspects of renewable energy and computing. The main aim of this conference will be to provide an international premier forum for the presentation of new advances and research results in the field of Green Energy, Computing and security, sustainable development, Artificial intelligence and Embedded Systems. The focus area and tracks of the conference would be:</p>
-            <p><span className="bold-text">Track 1:</span> Green Energy, Electric Drives and Smart Grid<br />
-              <span className="bold-text">Track 2:</span> Computing and Security<br />
-              <span className="bold-text">Track 3:</span> Sustainable Technologies and Development<br />
-              <span className="bold-text">Track 4:</span> Artificial Intelligence and Embedded Systems</p>
+            <p>The international conference on  Advanced Computing and Emerging Technologies (AKGEC-2023) will offer themes in the engineering aspects of renewable energy and computing. The main aim of this conference will be to provide an international premier forum for the presentation of new advances and research results in the field of Green Energy, Computing and security, sustainable development, Artificial intelligence and Embedded Systems. The focus area and tracks of the conference would be:</p>
+            <p><span className="bold-text">Track 1:</span> Advances in Machine Learning and Deep Learning<br />
+              <span className="bold-text">Track 2:</span> Advances in Data Science<br />
+              <span className="bold-text">Track 3:</span> Applications of Artificial Intelligence in Interdisciplinary areas<br />
+              <span className="bold-text">Track 4:</span> High Performance Computing<br />
+              <span className="bold-text">Track 5:</span> Advances in Communication and Networks<br />
+              <span className="bold-text">Track 6:</span> Advanced Algorithms<br />
+              <span className="bold-text">Track 7:</span> Teaching & Learning Systems</p>
           </div>
         </div>
 
