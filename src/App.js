edac07f98f373f92453591cd38cd23644500ef44
hargun0360 <hargunsingh0720@gmail.com>
2022-11-15 20:30:17 +0530

    Change committee members

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
           <Route path='/call-for-paper' component={Call} />
           <Route path='/keynote-speakers' component={Keynote} />
           <Route path="/registration" component={Registration} />
-          {/* <Route path='/committee' component={Committee} /> */}
+          <Route path='/committee' component={Committee} />
           <Route path='/accomodation' component={Award} />
         </Switch>
       </div>
